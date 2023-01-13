c59766f837cd14c7b5638abb17fc0defc05ab14f
VVladyslav <VVVasylyshyn@gmail.com>
2023-01-13 09:53:07 +0200

    hv-07
    prettier applied

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,9 +1,17 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-const gallery = document.querySelector('.gallery');
-const markup = galleryItems.reduce((acc, {original, preview, description}) => acc + `<a class="gallery__item" href="${original}">
-<img class="gallery__image" src="${preview}" alt="${description}"/></a>`,'');
-gallery.insertAdjacentHTML('afterbegin', markup);
+const gallery = document.querySelector(".gallery");
+const markup = galleryItems.reduce(
+  (acc, { original, preview, description }) =>
+    acc +
+    `<a class="gallery__item" href="${original}">
+<img class="gallery__image" src="${preview}" alt="${description}"/></a>`,
+  ""
+);
+gallery.insertAdjacentHTML("afterbegin", markup);
 
-let instance = new SimpleLightbox('.gallery.gallery a', {captionsData: 'alt' , captionDelay: '250'});
+let instance = new SimpleLightbox(".gallery.gallery a", {
+  captionsData: "alt",
+  captionDelay: "250",
+});
 instance.on(`show.instance`);
